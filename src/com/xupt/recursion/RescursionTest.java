7b6a32319d1419c0e13497e2481fb3ce4820fcbe
wangwenhandsome <wangwen_0513@163.com>
2020-04-17 15:22:49 +0800

    递归

--- src/com/xupt/recursion/RescursionTest.java
+++ src/com/xupt/recursion/RescursionTest.java
@@ -10,10 +10,10 @@ package com.xupt.recursion;
 public class RescursionTest {
     public static void main(String[] args) {
         //通过打印问题，回顾递归调用机制
-        test(4);
+//        test(4);
 
-        //int res = factorial(3);
-        //System.out.println("res=" + res);
+        int res = factorial(19);
+        System.out.println("res=" + res);
     }
     //打印问题.
     public static void test(int n) {
