172a614e46d7ad2c6e43e9e7d043f858a93cd56e
wangwenhandsome <wangwen_0513@163.com>
2020-04-19 10:22:30 +0800

    排序

--- src/com/xupt/sort/InsertSort.java
+++ src/com/xupt/sort/InsertSort.java
@@ -1,6 +1,8 @@
 package com.xupt.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * @Author: 王文
@@ -11,8 +13,26 @@ import java.util.Arrays;
 
 public class InsertSort {
     public static void main(String[] args) {
-        int[] arr = {101, 34, 119, 1, -1, 89};
-        insertSort(arr);
+        //int[] arr = {101, 34, 119, 1, -1, 89};
+        // 创建要给80000个的随机的数组
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+        }
+
+        System.out.println("插入排序前");
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前的时间是=" + date1Str);
+
+        insertSort(arr); //调用插入排序算法
+
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序前的时间是=" + date2Str);
+
+        //System.out.println(Arrays.toString(arr));
     }
     //插入排序
     public  static void insertSort(int[] arr){
@@ -33,10 +53,13 @@ public class InsertSort {
             }
             //当退出while循环时，说明插入的位置找到, insertIndex + 1
             //举例：理解不了，我们一会 debug
-            arr[insertIndex + 1] = insertVal;
+            //这里我们判断是否需要赋值
+            if(insertIndex + 1 != i) {
+                arr[insertIndex + 1] = insertVal;
+            }
 
-            System.out.println("第"+i+"轮插入");
-            System.out.println(Arrays.toString(arr));
+//            System.out.println("第"+i+"轮插入");
+//            System.out.println(Arrays.toString(arr));
         }
 
 
--- src/com/xupt/sort/SelectSort.java
+++ src/com/xupt/sort/SelectSort.java
@@ -1,6 +1,8 @@
 package com.xupt.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * @Author: 王文
@@ -11,8 +13,26 @@ import java.util.Arrays;
 
 public class SelectSort {
     public static void main(String[] args) {
-    int [] arr = {101, 34, 119, 1, -1, 90, 123};
-    selectSort(arr);
+        //int[] arr = {101, 34, 119, 1, -1, 89};
+        // 创建要给80000个的随机的数组
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+        }
+
+        System.out.println("选择排序前");
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前的时间是=" + date1Str);
+
+        selectSort(arr); //调用插入排序算法
+
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序前的时间是=" + date2Str);
+
+        //System.out.println(Arrays.toString(arr));
     }
 
     //选择排序
@@ -34,8 +54,8 @@ public class SelectSort {
                arr[minIndex]=arr[i];
                arr[i]=min;
             }
-            System.out.println("第"+(i+1)+"轮后~~");
-            System.out.println(Arrays.toString(arr));// 1, 34, 119, 101
+//            System.out.println("第"+(i+1)+"轮后~~");
+//            System.out.println(Arrays.toString(arr));// 1, 34, 119, 101
         }
 
         //使用逐步推导的方式来，讲解选择排序
