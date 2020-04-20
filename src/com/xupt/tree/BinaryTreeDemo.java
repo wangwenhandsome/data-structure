784c1d6033d2c4750a728c6d5e4d07877a85db61
wangwenhandsome <wangwen_0513@163.com>
2020-04-20 21:23:56 +0800

    哈希表

---
+++ src/com/xupt/tree/BinaryTreeDemo.java
@@ -0,0 +1,77 @@
+package com.xupt.tree;
+
+/**
+ * @Author: 王文
+ * @Date: 2020/4/20 20:22
+ * @Version: 1.0
+ * @Description:
+ */
+
+public class BinaryTreeDemo {
+    public static void main(String[] args) {
+
+    }
+}
+//先创建HeroNode节点
+class HeroNode{
+    private int no;
+    private String name;
+    private HeroNode left;
+    private HeroNode right;
+
+    public HeroNode(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HeroNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(HeroNode left) {
+        this.left = left;
+    }
+
+    public HeroNode getRight() {
+        return right;
+    }
+
+    public void setRight(HeroNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                '}';
+    }
+    //编写前序遍历的方法
+    public void preOrder() {
+        System.out.println(this); //先输出父结点
+        //递归向左子树前序遍历
+
+        //递归向右子树前序遍历
+
+
+    }
+   //中序遍历
+    //后序遍历
+}
