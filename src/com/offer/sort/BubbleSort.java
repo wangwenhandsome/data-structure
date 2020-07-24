2ce1559641faf6b371e91d6c9cd5f3c1680d0eb5
wangwenhandsome <wangwen_0513@163.com>
2020-07-24 19:15:10 +0800

    哈希表

--- src/com/offer/sort/BubbleSort.java
+++ src/com/offer/sort/BubbleSort.java
@@ -15,8 +15,8 @@ public class BubbleSort {
         bubblesort(a);
     }
     public static void bubblesort(int [] arr){
-        for (int i=0;i<arr.length;i++){
-            for(int j=i;j<arr.length-1;j++) {
+        for (int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
