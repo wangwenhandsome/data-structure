517a3986f2629dcbe11eb20b2ea8ea3bd7ecb028
wangwenhandsome <wangwen_0513@163.com>
2020-08-03 15:44:53 +0800

    排序

--- src/com/offer/sort/ShellSort.java
+++ src/com/offer/sort/ShellSort.java
@@ -1,5 +1,7 @@
 package com.offer.sort;
 
+import java.util.Arrays;
+
 /**
  * @Author: 王文
  * @Date: 2020/8/3 15:33
@@ -8,4 +10,25 @@ package com.offer.sort;
  */
 
 public class ShellSort {
+    public static void main(String[] args) {
+        int arr[]={8,9,1,7,2,3,5,4,6,0};
+        shellSort1(arr);//交换方式
+    }
+    public static void shellSort1(int [] arr){
+        int temp;
+        int count=0;
+        for (int gap=arr.length/2;gap>0;gap/=2){
+            for (int i=gap;i<arr.length;i++){
+                for (int j=i-gap;j>=0;j-=gap){
+                    if (arr[j]>arr[j+gap]){
+                        temp=arr[j];
+                        arr[j]=arr[j+gap];
+                        arr[j+gap]=temp;
+                    }
+                }
+            }
+            System.out.println("希尔排序第" + (++count) + "轮 =" + Arrays.toString(arr));
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
