490a502d55d488f0ec84b1dc2fbf906f0c2f70d6
wangwenhandsome <wangwen_0513@163.com>
2020-08-03 18:07:10 +0800

    快速排序

---
+++ src/com/offer/Test/Test4.java
@@ -0,0 +1,34 @@
+package com.offer.Test;
+
+import java.util.Arrays;
+
+/**
+ * @Author: 王文
+ * @Date: 2020/8/3 17:27
+ * @Version: 1.0
+ * @Description:
+ */
+
+public class Test4 {
+    public static void main(String[] args) {
+        int arr[]={8,9,1,7,2,3,5,4,6,0};
+        shellSort2(arr);//交换方式
+    }
+    public static void shellSort2(int [] arr){
+        int temp;
+        for (int gap=arr.length/2;gap>0;gap/=2){
+            for (int i=gap;i<arr.length;i++){
+                int j=i;
+                temp=arr[i];
+                while (j-gap>=0&&temp<arr[j-gap]){
+                    arr[j]=arr[j-gap];
+                    j-=gap;
+                }
+                if(j!=i){
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
