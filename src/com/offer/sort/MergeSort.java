8557c7080d25f926478bad81a2e6547fc77e054e
wangwenhandsome <wangwen_0513@163.com>
2020-08-04 10:47:10 +0800

    归并排序

--- src/com/offer/sort/MergeSort.java
+++ src/com/offer/sort/MergeSort.java
@@ -1,25 +1,24 @@
-package com.xupt.sort;
+package com.offer.sort;
 
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 
 /**
  * @Author: 王文
- * @Date: 2020/4/19 19:17
+ * @Date: 2020/8/4 10:11
  * @Version: 1.0
  * @Description:
  */
 
-public class MergetSort {
+public class MergeSort {
     public static void main(String[] args) {
-        int arr[] = { 8, 4, 5, 7, 1, 3, 6, 2 }; //
+        int arr[] = { 8, 4, 5, 7, 1, 3, 6, 2 };
         int temp[] = new int[arr.length]; //归并排序需要一个额外空间
         mergeSort(arr, 0, arr.length - 1, temp);
+
         System.out.println("归并排序后=" + Arrays.toString(arr));
     }
     //分+合方法
-    public static void mergeSort(int[] arr, int left, int right, int[] temp) {
+    public static void mergeSort(int []arr,int left,int right,int [] temp){
         if(left < right) {
             int mid = (left + right) / 2; //中间索引
             //向左递归进行分解
@@ -40,44 +39,39 @@ public class MergetSort {
      * @param right 右边索引
      * @param temp 做中转的数组
      */
-    public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
-
-        int i = left; // 初始化i, 左边有序序列的初始索引
-        int j = mid + 1; //初始化j, 右边有序序列的初始索引
-        int t = 0; // 指向temp数组的当前索引
-
+    public static void merge(int [] arr, int left , int mid, int right, int [] temp){
+        int i=left; //初始化i,左边有序序列的起始地址
+        int j=mid+1;//初始化j,右边有序序列的起始地址
+        int t=0;    //临时数组的起始地址
         //(一)
         //先把左右两边(有序)的数据按照规则填充到temp数组
         //直到左右两边的有序序列，有一边处理完毕为止
-        while(i<=mid&&j<=right){//继续
+        while (i<=mid&&j<=right){
             //如果左边的有序序列的当前元素，小于等于右边有序序列的当前元素
             //即将左边的当前元素，填充到 temp数组
             //然后 t++, i++
-           if (arr[i]<=arr[j]){
-               temp[t]=arr[i];
-               t++;
-               i++;
-           }else{
-               temp[t]=arr[j];
-               t++;
-               j++;
-           }
+            if (arr[i]<=arr[j]){
+                temp[t]=arr[i];
+                t++;
+                i++;
+            }else{
+                temp[t]=arr[j];
+                t++;
+                j++;
+            }
         }
         //(二)
         //把有剩余数据的一边的数据依次全部填充到temp
-        while( i <= mid) { //左边的有序序列还有剩余的元素，就全部填充到temp
-            temp[t] = arr[i];
-            t += 1;
-            i += 1;
+        while (i<=mid){ //左边的有序序列还有剩余的元素，就全部填充到temp
+            temp[t]=arr[i];
+            t++;
+            i++;
         }
-
-        while( j <= right) { //右边的有序序列还有剩余的元素，就全部填充到temp
-            temp[t] = arr[j];
-            t += 1;
-            j += 1;
+        while (j<=right){//右边的有序序列还有剩余的元素，就全部填充到temp
+            temp[t]=arr[j];
+            j++;
+            t++;
         }
-
-
         //(三)
         //将temp数组的元素拷贝到arr
         //注意，并不是每次都拷贝所有
@@ -85,10 +79,10 @@ public class MergetSort {
         int tempLeft = left; //
         //第一次合并 tempLeft = 0 , right = 1 //  tempLeft = 2  right = 3 // tL=0 ri=3
         //最后一次 tempLeft = 0  right = 7
-        while(tempLeft <= right) {
-            arr[tempLeft] = temp[t];
-            t += 1;
-            tempLeft += 1;
+        while (tempLeft<=right){
+            arr[tempLeft]=temp[t];
+            tempLeft++;
+            t++;
         }
     }
 }
